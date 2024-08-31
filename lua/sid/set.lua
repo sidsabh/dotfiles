dd52cb41abb119839f73a5eb8c78a78a980c07f3
Sid Sabhnani <ssabhnani@utexas.edu>
2024-08-31 15:36:22 -0500

    a theme that works with ssh

--- lua/sid/set.lua
+++ lua/sid/set.lua
@@ -2,7 +2,7 @@ vim.opt.background = "dark"
 vim.api.nvim_create_autocmd("User", {
     pattern = "LazyDone",
     callback = function()
-        pcall(vim.cmd, "colorscheme palenight")
+        pcall(vim.cmd, "colorscheme catppuccin")
     end,
 })
 
