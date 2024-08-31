2155bea6e49c8b911409fd66256983bcd707b265
Sid Sabhnani <ssabhnani@utexas.edu>
2024-08-31 15:39:11 -0500

    vim settings

--- lua/sid/set.lua
+++ lua/sid/set.lua
@@ -1,4 +1,4 @@
-vim.opt.background = "dark"
+-- vim.opt.background = "dark"
 vim.api.nvim_create_autocmd("User", {
     pattern = "LazyDone",
     callback = function()
@@ -6,4 +6,11 @@ vim.api.nvim_create_autocmd("User", {
     end,
 })
 
+vim.opt.nu = true -- line numbers
 
+vim.opt.hlsearch = false -- turns off annoying select
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8 -- keeps lines on bottom when scroling
