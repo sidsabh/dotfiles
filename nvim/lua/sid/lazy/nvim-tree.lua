d8c1f72258fbca38432af65bee893ef4d9306a55
Sid Sabhnani <ssabhnani@utexas.edu>
2024-09-11 03:05:39 -0500

    launch neotree at open

--- nvim/lua/sid/lazy/nvim-tree.lua
+++ nvim/lua/sid/lazy/nvim-tree.lua
@@ -6,5 +6,17 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+    },
+    init = function()
+      if vim.fn.argc(-1) == 1 then
+        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        if stat and stat.type == "directory" then
+          require("neo-tree").setup({
+            filesystem = {
+              hijack_netrw_behavior = "open_current",
+            },
+          })
+        end
+      end
+    end
 }
